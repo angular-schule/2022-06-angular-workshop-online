4b3b15a8c0acbacb3e7376bac873d4a56bb72b70
Ferdinand Malcher <ferdinand@malcher.media>
2022-06-22 10:44:20 +0200

    TypeScript Playground

--- book-rating/src/main.ts
+++ book-rating/src/main.ts
@@ -10,3 +10,41 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+
+  ///////////////////
+
+export class Customer {
+  /*id: number;
+
+  constructor(id: number) {
+    this.id = id;
+  }*/
+
+  constructor(public id: number) {}
+
+  fooBar(foo: string): string {
+    setTimeout(() => {
+      console.log('ID', this.id);
+    }, 2000);
+
+    return '';
+  }
+}
+
+const myCustomer = new Customer(6);
+
+console.log(myCustomer);
+myCustomer.fooBar('');
+
+///////////////
+
+/*
+function (param) {
+  return param + 1;
+}
+
+param => {
+  return param + 1;
+}
+*/
