f6b266d784cd899d83219e7666fb085626391c9a
Ferdinand Malcher <ferdinand@malcher.media>
2022-06-23 14:46:00 +0200

    Service nutzen / Buchliste neu erstellen

--- book-rating/src/app/books/dashboard/dashboard.component.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Book } from '../shared/book';
+import { BookRatingService } from '../shared/book-rating.service';
 
 @Component({
   selector: 'br-dashboard',
@@ -10,7 +11,7 @@ export class DashboardComponent implements OnInit {
 
   books: Book[] = [];
 
-  constructor() {
+  constructor(private rs: BookRatingService) {
     this.books = [
       {
         isbn: '123',
@@ -30,11 +31,28 @@ export class DashboardComponent implements OnInit {
   }
 
   doRateUp(book: Book) {
-    console.log('UP', book);
+    const ratedBook = this.rs.rateUp(book);
+    this.updateList(ratedBook);
   }
 
   doRateDown(book: Book) {
-    console.log('DOWN', book);
+    const ratedBook = this.rs.rateDown(book);
+    this.updateList(ratedBook);
+  }
+
+  private updateList(ratedBook: Book) {
+    // [1,2,3,4,5,6].filter(e => e > 3) // [4, 5, 6]
+    // [1,2,3,4,5,6].map(e => e * 10) // [10, 20, 30, 40, 50, 60]
+
+    /*this.books.map(b => {
+      if (b.isbn === ratedBook.isbn) {
+        return ratedBook;
+      } else {
+        return b;
+      }
+    })*/
+
+    this.books = this.books.map(b => b.isbn === ratedBook.isbn ? ratedBook : b);
   }
 
   ngOnInit(): void {}
