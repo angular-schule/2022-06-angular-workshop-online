7007b67283aba772521afe11b51367b7816e9c67
Ferdinand Malcher <ferdinand@malcher.media>
2022-06-28 12:31:29 +0200

    Creating Observables

--- rxjs-playground/src/app/exercises/creating/creating.component.ts
+++ rxjs-playground/src/app/exercises/creating/creating.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Observable, of, from, timer, interval, ReplaySubject, map, filter } from 'rxjs';
+import { Observable, of, from, timer, interval, ReplaySubject, map, filter, Observer, Subscriber } from 'rxjs';
 
 @Component({
   selector: 'rxw-creating',
@@ -22,7 +22,61 @@ export class CreatingComponent {
 
     /******************************/
 
-    
+    /*new Observable(sub => {
+      sub.next(1);
+      sub.next(2);
+      sub.next(3);
+      sub.next(4);
+      sub.next(5);
+      sub.complete();
+    })*/
+
+    // of(1,2,3,4,5)
+    // from([1,2,3,4,5])
+    // interval(1000) // ---0---1---2---3---4---5---...
+    // timer(2000) // ------0|
+    // timer(3000, 1000) // ---------0---1---2---3---4---5---...
+    // timer(0, 1000) // 0---1---2---3---4---5---...
+
+    timer(0, 1000).subscribe({
+      next: e => this.log(e),
+      complete: () => this.log('COMPLETE')
+    });
+
+
+    /******************************/
+
+
+    function producer(subscriber: Subscriber<number>) {
+      subscriber.next(Math.random());
+      subscriber.next(2);
+      const timer1 = setTimeout(() => subscriber.next(3), 2000);
+      setTimeout(() => subscriber.complete(), 3000);
+
+      // Teardown Logic
+      return () => {
+        clearTimeout(timer1);
+      };
+    }
+
+    const observer: Observer<number> = {
+      next: value => {
+        console.log(value);
+      },
+      error: (err: any) => {
+        console.error(err)
+      },
+      complete: () => {
+        console.log('COMPLETE');
+      }
+    }
+
+    // producer(observer);
+    // Finnische Notation
+    const myObservable$ = new Observable(producer);
+    // myObservable$.subscribe(observer);
+
+
     /******************************/
   }
 
